af40a0467d9373ed49c1bb0a8684c2023750575f
captainerd <58100748+captainerd@users.noreply.github.com>
2019-11-25 19:14:33 +0200

    Update class.Swoole_Srv.php
    added deflate, 
    use of internal swoole_get_mime_type()

--- includes/class.Swoole_Srv.php
+++ includes/class.Swoole_Srv.php
@@ -1,26 +1,36 @@
 <?php
 
 class Swoole_Srv {
-    private $fd;
-    private $ser;
+    private $response;
+    private $request;
     private $local_uri;
 
-    public function __construct($fd, $ser) {
-        $this->fd = $fd;
-        $this->ser = $ser;
+    public function __construct($response, $request) {
+        $this->response = $response;
+        $this->request = $request;
 
     }
     
-  public  function handleRequest($wwwDir) {
+  public  function get_request() {  
+    return $this->request;
+
+  }
+  public  function get_response() {  
+    return $this->response;
+
+  }
+  public  function handle_Request($wwwDir) {
  
-    $this->local_uri = $wwwDir.$this->ser->server['request_uri'];
+    $this->local_uri = $wwwDir.$this->request->server['request_uri'];
       //lets see if its dir look for index.html, htm
-      if ($this->ser->server['request_uri'] == "/") {
-        $this->local_uri = $this->local_uri.'index.html';
+      if ($this->request->server['request_uri'] == "/") {
+
+        if (file_exists($this->local_uri.'index.html')) $this->local_uri = $this->local_uri.'index.html';
+        if (file_exists($this->local_uri.'index.htm')) $this->local_uri = $this->local_uri.'index.htm';
       }
        if (is_dir($this->local_uri) && substr($this->local_uri,strlen($this->local_uri)-1,1) != "/") {
-         $this->fd->header('Location', $this->ser->server['request_uri'].'/');
-         $this->fd->status(301);
+         $this->response->header('Location', $this->request->server['request_uri'].'/');
+         $this->response->status(301);
           return;
        }
       
@@ -30,51 +40,48 @@ class Swoole_Srv {
        if (file_exists($this->local_uri) && !is_dir($this->local_uri)) { 
         $this->SendFile($this->local_uri);
        } else {
-        $this->fd->status(404);
-        $this->fd->end('Not found ');
+        $this->response->status(404);
+        $this->response->end('Not found ');
        }
 }
 
 public function SendFile($filename) {
-
-if (strpos(strtolower($this->ser->header['accept-encoding']),'gzip') != false) {
-    $this->fd->header("Content-Encoding", "gzip");
-    $this->fd->status(200);
+ 
+if (isset($this->request->header['accept-encoding']) && strpos(strtolower($this->request->header['accept-encoding']),'gzip') != false) {
+    $this->response->header("Content-Encoding", "gzip");
+    $this->response->status(200);
     $dsend = gzencode(file_get_contents($filename));
 } else {
    $dsend = file_get_contents($filename);
 }
-$this->fd->header("Content-Type", $this->swoole_mime_type($filename));
-$this->fd->end($dsend);
+$this->response->header("Content-Type", swoole_get_mime_type($filename));
+$this->response->end($dsend);
 
 }
-public function SendResponse($response, $type, int $status) {
-  $mimet = json_decode(file_get_contents('includes/mimetypes.txt'), true);
-    if (strpos(strtolower($this->ser->header['accept-encoding']),'gzip') != false) {
-        $this->fd->header("Content-Encoding", "gzip");
+public function send_response($response, $type, int $status = 0) {
+$gziped = false;
+$type = "res.".$type;
+$type = swoole_get_mime_type('ext'.strtolower($type));
+ 
+    if (isset($this->request->header['accept-encoding']) && strpos(strtolower($this->request->header['accept-encoding']),'gzip') != false) {
+        $this->response->header("Content-Encoding", "gzip");
         $response = gzencode($response);
+        $gziped = true;
     }  
-  
-    $this->fd->status($status);
-    $this->fd->header("Content-Type", $mimet[$type]);
-    $this->fd->end($response);
+    if ($gziped == false && isset($this->request->header['accept-encoding']) && strpos(strtolower($this->request->header['accept-encoding']),'deflate') != false) {
+      $this->response->header("Content-Encoding", "deflate");
+      $response = gzdeflate($response);
+      $gziped = true;
+  }  
+
+    if ($status != 0) $this->response->status($status);
+    $this->response->header("Content-Type", $type);
+    $this->response->header("requestver", "ScoopScript");
+    $this->response->end($response);
     
 }
 
-private function swoole_mime_type($filename) {
  
-    $idx = explode( '.', $filename );
-    $count_explode = count($idx);
-    $idx = strtolower($idx[$count_explode-1]);
-
-    $mimet = json_decode(file_get_contents('includes/mimetypes.txt'), true);
-
-    if (isset( $mimet[$idx] )) {
-     return $mimet[$idx];
-    } else {
-     return 'application/octet-stream';
-    }
- }
 }
 
 
